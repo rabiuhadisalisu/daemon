a7249b095a6048f56c3370cf1a4eae494b8f8ee1
Rabiu Hadi Salisu <94430863+rabiuhadisalisu@users.noreply.github.com>
2024-07-30 01:09:48 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask
 from threading import Thread
-app = Flask(__name__)
+import os
+
+app = Flask('')
+
 @app.route('/')
-def index():
-    return os.environ.get('EMAIL')
+def home():
+    return os.environ.get('EMAIL', 'I am alive!')
+
 def run():
-  app.run(host='0.0.0.0',port=80)
-def keep_alive():  
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=80)
+
+def keep_alive():
     t = Thread(target=run)
     t.start()
